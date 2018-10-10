270e7faeaad8da911de2e0b30f15ce697f6c4874
Nathan Dickson <nathan.dickson@uos.ac.uk>
2018-10-10 14:39:50 +0100

    Nathan

--- src/main/java/uk/ac/uos/i2p/SimpleForGit.java
+++ src/main/java/uk/ac/uos/i2p/SimpleForGit.java
@@ -1,7 +1,7 @@
 package uk.ac.uos.i2p;
 
 public class SimpleForGit {
-
+ //Every one loves Nathan 
 	public static void main(String[] args) {
 		SimpleForGit sg = new SimpleForGit();
 		System.err.println("Hello from " + sg.getName());
